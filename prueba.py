fe46c032aaf666b449a85ce78fcfb752ad28a835
Francoco97 <fpalmatrejo@gmail.com>
2020-11-22 18:16:08 -0300

    Create prueba.py

---
+++ prueba.py
@@ -0,0 +1,78 @@
+
+import imaplib
+import email
+from email.header import decode_header
+import webbrowser
+import os
+
+# account credentials
+username = "fpalmatrejo@gmail.com"
+password = "personal12397"
+
+# create an IMAP4 class with SSL
+imap = imaplib.IMAP4_SSL("imap.gmail.com")
+# authenticate
+imap.login(username, password)
+
+status, messages = imap.select("INBOX")
+# number of top emails to fetch
+N = 50
+# total number of emails
+messages = int(messages[0])
+
+for i in range(messages, messages-N, -1):
+    # fetch the email message by ID
+    res, msg = imap.fetch(str(i), "(RFC822)")
+    for response in msg:
+        if isinstance(response, tuple):
+            # parse a bytes email into a message object
+            msg = email.message_from_bytes(response[1])
+            # decode the email subject
+            subject = decode_header(msg["Subject"])[0][0]
+            if isinstance(subject, bytes):
+                # if it's a bytes, decode to str
+                subject = subject.decode()
+            # decode email sender
+            From, encoding = decode_header(msg.get("From"))[0]
+            if isinstance(From, bytes):
+                From = From.decode(encoding)
+            MngId, encoding = decode_header(msg.get("Message-ID"))[0]
+            if isinstance(MngId, bytes):
+                MngId = MngId.decode(encoding)
+            if From == "noreply@bancoestado.cl":
+                print("Subject:", subject)
+                print("From:", From)
+                print(From)
+                print("Mng-ID", MngId)
+                # if the email message is multipart
+                if msg.is_multipart():
+                    # iterate over email parts
+                    for part in msg.walk():
+                        # extract content type of email
+                        content_type = part.get_content_type()
+                        content_disposition = str(part.get("Content-Disposition"))
+                        try:
+                            # get the email body
+                            body = part.get_payload(decode=True).decode()
+                        except:
+                            pass
+                        if content_type == "text/plain" and "attachment" not in content_disposition:
+                            # print text/plain emails and skip attachments
+                            print(body)
+                        elif "attachment" in content_disposition:
+                            pass
+                else:
+                    # extract content type of email
+                    content_type = msg.get_content_type()
+                    # get the email body
+                    body = msg.get_payload(decode=True).decode()
+                    if content_type == "text/plain":
+                        # print only text email parts
+                        print(body)
+                if content_type == "text/html":
+                    pass
+                print("="*100)
+# close the connection and logout
+print("="*26+"No se encontraron mas correos de transferencias."+"="*26)
+imap.close()
+imap.logout()
